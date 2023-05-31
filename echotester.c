c28f65ef6e139349fec3afc66f46704a9bba6d45
Sarah Watt <contact@sarahwatt.ca>
2023-05-31 00:18:10 -0400

    Update echotester.c
    Better handling of certain errors, better results reporting, poll timeout added and better handling of the timerfd ticking

--- echotester.c
+++ echotester.c
@@ -53,6 +53,7 @@ enum arg_keys
 	KEY_DURATION = 'd',
 	KEY_LENGTH = 'l',
 	KEY_PORT = 'p',
+	KEY_TIMEOUT = 't',
 	KEY_WORKERS = 'w'
 };
 
@@ -63,6 +64,7 @@ static struct argp_option argp_options[] =
 	{"duration",	KEY_DURATION,	"NUMBER",		0,			"Duration of test in seconds (default 60)"},
 	{"length",		KEY_LENGTH,		"NUMBER",		0,			"Length of message in bytes (default 32)"},
 	{"port",		KEY_PORT,		"NUMBER",		0,			"Network port to use (default 8080)"},
+	{"timeout",		KEY_TIMEOUT,	"NUMBER",		0,			"Time to wait for socket state change before giving up in milliseconds (default 1000)"},
 	{"workers",		KEY_WORKERS,	"NUMBER",		0,			"Number of worker processes (default 1)"},
 	{0}
 };
@@ -74,6 +76,7 @@ struct arguments
 	int duration;
 	int length;
 	unsigned short port;
+	int timeout;
 	int workers;
 };
 
@@ -96,6 +99,9 @@ static error_t argp_parse_options(int key, char* arg, struct argp_state* state)
 	case KEY_PORT:
 		sscanf(arg, "%hu", &args->port);
 		break;
+	case KEY_TIMEOUT:
+		sscanf(arg, "%i", &args->timeout);
+		break;
 	case KEY_WORKERS:
 		sscanf(arg, "%i", &args->workers);
 		break;
@@ -112,6 +118,7 @@ static error_t argp_parse_options(int key, char* arg, struct argp_state* state)
 // smalloc asks for a little extra memory so it can prepend a size,
 // which is used by sfree when later being freed
 // *********************************************************************
+
 void* smalloc(size_t size)
 {
 	if (size == 0)
@@ -177,8 +184,17 @@ struct process_entry
 	int status;
 };
 
+struct result
+{
+	long total;
+	long successful;
+	long timeout;
+	long mismatchLength;
+	long mismatchContents;
+};
+
 struct process_entry* processTable = 0;
-long* shared = 0;
+struct result* results = 0;
 
 void cleanup()
 {
@@ -187,9 +203,9 @@ void cleanup()
 		free(processTable);
 	}
 	
-	if (shared)
+	if (results)
 	{
-		sfree(shared);
+		sfree(results);
 	}
 }
 
@@ -201,7 +217,11 @@ void process(int id, struct arguments* args)
 	int retval;
 	
 	// Initialize success counter in shared memory
-	shared[id] = 0;
+	results[id].total = 0;
+	results[id].successful = 0;
+	results[id].timeout = 0;
+	results[id].mismatchLength = 0;
+	results[id].mismatchContents = 0;
 	
 	// Allocate buffers
 	size_t txBufferSize = args->length;
@@ -209,7 +229,7 @@ void process(int id, struct arguments* args)
 	
 	if (txBuffer == NULL)
 	{
-		fprintf(stderr, "Error: Cannot allocate transmit buffer for worker #%i: %s\n", id, strerror(errno));
+		fprintf(stderr, "Error: Worker #%i cannot allocate transmit buffer: %s\n", id, strerror(errno));
 		exit(EXIT_FAILURE);
 	}
 	
@@ -221,7 +241,7 @@ void process(int id, struct arguments* args)
 	
 	if (rxBuffer == NULL)
 	{
-		fprintf(stderr, "Error: Cannot allocate receive buffer for worker #%i: %s\n", id, strerror(errno));
+		fprintf(stderr, "Error: Worker #%i cannot allocate receive buffer: %s\n", id, strerror(errno));
 		exit(EXIT_FAILURE);
 	}
 	
@@ -256,7 +276,7 @@ void process(int id, struct arguments* args)
 	
 	if (timerfd < 0)
 	{
-		fprintf(stderr, "Error: Cannot create timerfd for worker #%i: %s\n", id, strerror(errno));
+		fprintf(stderr, "Error: Worker #%i cannot create timerfd: %s\n", id, strerror(errno));
 		exit(EXIT_FAILURE);
 	}
 	
@@ -264,7 +284,7 @@ void process(int id, struct arguments* args)
 	
 	if (retval < 0)
 	{
-		fprintf(stderr, "Error: Cannot set timerfd time for worker #%i: %s\n", id, strerror(errno));
+		fprintf(stderr, "Error: Worker #%i cannot set timerfd time: %s\n", id, strerror(errno));
 		exit(EXIT_FAILURE);
 	}
 	
@@ -276,12 +296,14 @@ void process(int id, struct arguments* args)
 	
 	while (1)
 	{
+		results[id].total++;
+		
 		// Create socket
 		int sockfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, IPPROTO_TCP);
 		
 		if (sockfd < 0)
 		{
-			fprintf(stderr, "Error: Cannot create socket for worker #%i: %s\n", id, strerror(errno));
+			fprintf(stderr, "Error: Worker #%i cannot open socket: %s\n", id, strerror(errno));
 			exit(EXIT_FAILURE);
 		}
 		
@@ -303,23 +325,30 @@ void process(int id, struct arguments* args)
 		
 		while (1)
 		{
-			retval = poll(poll_list, 2, -1);
+			retval = poll(poll_list, 2, args->timeout);
 			
 			if (retval < 0)
 			{
-				fprintf(stderr, "Error: Worker #%i has problem polling: %s\n", id, strerror(errno));
+				fprintf(stderr, "Error: Worker #%i cannot poll FDs: %s\n", id, strerror(errno));
 				exit(EXIT_FAILURE);
 			}
+			else if (retval == 0)
+			{
+				if (results[id].mismatchContents == 0)
+				{
+					fprintf(stderr, "Warning: Worker #%i timed out\n", id);
+				}
+				
+				results[id].timeout++;
+				
+				break;
+			}
 			
-			// If timerfd ticks, we're done
+			// If timerfd ticks, we're done after the next completion or timeout
 			if (poll_list[0].revents & POLLIN)
 			{
-				close(sockfd);
-				
-				free(txBuffer);
-				free(rxBuffer);
-				
-				return;
+				// This causes the timer FD entry in the poll list to be ignored by subsequent calls to poll
+				poll_list[0].fd = -1;
 			}
 			
 			// Check socket state
@@ -329,12 +358,12 @@ void process(int id, struct arguments* args)
 				
 				if (n < 0)
 				{
-					fprintf(stderr, "Error: Worker #%i has problem sending: %s\n", id, strerror(errno));
+					fprintf(stderr, "Error: Worker #%i cannot write to socket: %s\n", id, strerror(errno));
 					exit(EXIT_FAILURE);
 				}
 				else if (n != txBufferSize)
 				{
-					fprintf(stderr, "Error: Worker #%i unable to send full string\n", id);
+					fprintf(stderr, "Error: Worker #%i cannot write full string\n", id);
 					exit(EXIT_FAILURE);
 				}
 				
@@ -347,13 +376,19 @@ void process(int id, struct arguments* args)
 				// Compare message size
 				if (n < 0)
 				{
-					fprintf(stderr, "Error: Worker #%i has problem receiving: %s\n", id, strerror(errno));
+					fprintf(stderr, "Error: Worker #%i cannot read socket: %s\n", id, strerror(errno));
 					exit(EXIT_FAILURE);
 				}
 				else if (n != txBufferSize)
 				{
-					fprintf(stderr, "Error: Worker #%i has receive length mismatch\n", id);
-					exit(EXIT_FAILURE);
+					if (results[id].mismatchLength == 0)
+					{
+						fprintf(stderr, "Warning: Worker #%i has receive length mismatch\n", id);
+					}
+					
+					results[id].mismatchLength++;
+					
+					break;
 				}
 				
 				// Compare message contents
@@ -361,17 +396,34 @@ void process(int id, struct arguments* args)
 				
 				if (retval != 0)
 				{
-					fprintf(stderr, "Error: Worker #%i has receive content mismatch\n", id);
-					exit(EXIT_FAILURE);
+					if (results[id].mismatchContents == 0)
+					{
+						fprintf(stderr, "Warning: Worker #%i has receive content mismatch\n", id);
+					}
+					
+					results[id].mismatchContents++;
+					
+					break;
 				}
 				
-				close(sockfd);
-				
 				// Record a successful result
-				shared[id]++;
+				results[id].successful++;
 				
 				break;
 			}
+		}
+		
+		close(sockfd);
+		
+		// If the timerfd ticked, we're done
+		if (poll_list[0].fd < 0)
+		{
+			close(timerfd);
+			
+			free(txBuffer);
+			free(rxBuffer);
+			
+			return;
 		}
 	}
 }
@@ -393,6 +445,7 @@ int main(int argc, char* argv[])
 	args.duration = 60;
 	args.length = 32;
 	args.port = 8080;
+	args.timeout = 1000;
 	args.workers = 1;
 
 	// Parse arguments
@@ -403,6 +456,7 @@ int main(int argc, char* argv[])
 	printf("Port: %hu\n", args.port);
 	printf("Duration: %i seconds\n", args.duration);
 	printf("Length: %i bytes\n", args.length);
+	printf("Timeout: %i milliseconds\n", args.timeout);
 	printf("Workers: %i\n", args.workers);
 	
 	// Allocate a table to store process information in
@@ -415,9 +469,9 @@ int main(int argc, char* argv[])
 	}
 
 	// Allocate shared memory
-	shared = (long*)scalloc(args.workers, sizeof(long));
+	results = (struct result*)scalloc(args.workers, sizeof(struct result));
 	
-	if (shared == NULL)
+	if (results == NULL)
 	{
 		fprintf(stderr, "Error: Cannot allocate shared memory for results: %s\n", strerror(errno));
 		exit(EXIT_FAILURE);
@@ -481,14 +535,26 @@ int main(int argc, char* argv[])
 		}
 		
 		// Sum the results from workers that successfully exited
-		long result = 0;
+		struct result final =
+		{
+			.total = 0,
+			.successful = 0,
+			.timeout = 0,
+			.mismatchLength = 0,
+			.mismatchContents = 0
+		};
+		
 		int count = 0;
 		
 		for (id = 0; id < args.workers; id++)
 		{
 			if (processTable[id].status == 0)
 			{
-				result += shared[id];
+				final.total += results[id].total;
+				final.successful += results[id].successful;
+				final.timeout += results[id].timeout;
+				final.mismatchLength += results[id].mismatchLength;
+				final.mismatchContents += results[id].mismatchContents;
 
 				count++;
 			}
@@ -503,9 +569,26 @@ int main(int argc, char* argv[])
 			exit(EXIT_FAILURE);
 		}
 
-		// Do the final calculation and report the final result
-		printf("Number of successful echoes: %li\n", result);
-		printf("Rate of successful echoes: %li per second\n", result / args.duration);
+		// Do the final calculation and report the final results
+		printf("Number of attempts: %li\n", final.total);
+		printf("Rate of attempts: %li per second\n", final.total / args.duration);
+		printf("Number of successful echoes: %li\n", final.successful);
+		printf("Rate of successful echoes: %li per second\n", final.successful / args.duration);
+		
+		if (final.timeout > 0)
+		{
+			printf("Number of timeouts: %li\n", final.timeout);
+		}
+		
+		if (final.mismatchLength > 0)
+		{
+			printf("Number of length mismatches: %li\n", final.mismatchLength);
+		}
+		
+		if (final.mismatchContents > 0)
+		{
+			printf("Number of content mismatches: %li\n", final.mismatchContents);
+		}
 	}
 	
 	exit(EXIT_SUCCESS);
